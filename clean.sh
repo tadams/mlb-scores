df1e1711a57d446b12b24b7ab2809b21f3e9bedf
devops <devops@DocCi-Wrkshop.net>
2017-08-16 22:10:22 -0500

    add script to clean up VM

---
+++ clean.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# clear history
+history -c
+history -w
+
+# clean downloads
+rm ~/Downloads/*
+
+# clean firefox
+rm ~/.mozilla/firefox/*.default/cookies.sqlite
+rm ~/.mozilla/firefox/*.default/*.sqlite ~/.mozilla/firefox/*default/sessionstore.js
+rm -r ~/.cache/mozilla/firefox/*.default/*
+
+# remove all docker containers
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+# remove docker images
+docker rmi -f $(docker images -a -q)
+docker pull openjdk:alpine
+docker pull nginx:alpine
+
+# clean Jenkins jobs
+rm -rf /var/lib/jenkins/workspace/mlb-scores_DockerBuild
+rm -rf /var/lib/jenkins/workspace/mlb-scores_Deploy
+
+# clean build dir
+rm -rf ~/mlb-scores/build
