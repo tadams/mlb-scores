69a87fe7def5370aecde896b87d22b08c4862e3a
devops <devops@DocCi-Wrkshop.net>
2017-08-26 11:34:07 -0500

    update clean dirs for jenkins

--- clean.sh
+++ clean.sh
@@ -21,10 +21,14 @@ docker rmi -f $(docker images -a -q)
 docker pull openjdk:alpine
 docker pull nginx:alpine
 
+# clean build dir
+rm -rf ~/mlb-scores/build
+
 # clean Jenkins jobs
+sudo su jenkins
 rm -rf /var/lib/jenkins/workspace/mlb-scores_DockerBuild
 rm -rf /var/lib/jenkins/workspace/mlb-scores_Deploy
 rm -rf /var/lib/jenkins/workspace/mlb-scores_Build
-
-# clean build dir
-rm -rf ~/mlb-scores/build
+rm /var/lib/jenkins/logs/tasks/*
+rm -rf /var/lib/jenkins/jobs/mlb-scores_Build/builds
+exit
