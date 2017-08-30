5874da684ff863cc952e11a401d753e0dcb3d690
devops <devops@DocCi-Wrkshop.net>
2017-08-30 10:26:54 -0500

    update clean up script

--- clean.sh
+++ clean.sh
@@ -1,34 +1,54 @@
 #!/bin/sh
+set -e
 
 # clear history
-history -c
-history -w
+echo /dev/null > ~/.bash_history
 
 # clean downloads
-rm ~/Downloads/*
+rm -f ~/Downloads/*
 
 # clean firefox
-rm ~/.mozilla/firefox/*.default/cookies.sqlite
-rm ~/.mozilla/firefox/*.default/*.sqlite ~/.mozilla/firefox/*default/sessionstore.js
-rm -r ~/.cache/mozilla/firefox/*.default/*
+rm -f ~/.mozilla/firefox/*.default/cookies.sqlite
+rm -f ~/.mozilla/firefox/*.default/*.sqlite ~/.mozilla/firefox/*default/sessionstore.js
+rm -f -r ~/.cache/mozilla/firefox/*.default/*
 
 # remove all docker containers
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+CONTAINER_IDS="$(docker ps -a -q)"
+if [ -z "$CONTAINER_IDS" ]
+then
+  echo "No containers..."
+else 
+  docker stop $CONTAINER_IDS
+  docker rm $CONTAINER_IDS
+fi 
+
 
 # remove docker images
-docker rmi -f $(docker images -a -q)
+IMAGE_IDS="$(docker images -a -q)"
+if [ -z "$IMAGE_IDS" ]
+then
+  echo "No images..."
+else
+  docker rmi -f $IMAGE_IDS 
+fi 
+
 docker pull openjdk:alpine
 docker pull nginx:alpine
 
 # clean build dir
 rm -rf ~/mlb-scores/build
 
+# remove files created in workshop
+rm -f docker-compose.yml
+cd ~/mlb-scores/scripts
+rm -f docker*.sh
+
 # clean Jenkins jobs
-sudo su jenkins
 rm -rf /var/lib/jenkins/workspace/mlb-scores_DockerBuild
 rm -rf /var/lib/jenkins/workspace/mlb-scores_Deploy
 rm -rf /var/lib/jenkins/workspace/mlb-scores_Build
-rm /var/lib/jenkins/logs/tasks/*
+rm -f /var/lib/jenkins/logs/tasks/*
 rm -rf /var/lib/jenkins/jobs/mlb-scores_Build/builds
-exit
+
+
+
