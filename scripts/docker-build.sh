0f0b8c616fe54db0ee8066aa23d49fe79053cae4
devops <devops@DocCi-Wrkshop.net>
2017-08-31 14:14:18 -0500

    save git-describe in image label

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
-docker build -t ${DOCKER_HUB}/tca/nginx:${BUILD_NUMBER} ${WORKSPACE}/docker-nginx 
-
-docker build -t ${DOCKER_HUB}/tca/mlb-scores:${BUILD_NUMBER} ${WORKSPACE}/docker-mlb-scores
+docker build --tag ${DOCKER_HUB}/tca/nginx:${BUILD_NUMBER} \
+             --build-arg GIT_DESCRIBE=$(git describe)      \
+                   ${WORKSPACE}/docker-nginx
+ 
+docker build -t ${DOCKER_HUB}/tca/mlb-scores:${BUILD_NUMBER} \
+             --build-arg GIT_DESCRIBE=$(git describe)        \
+                  ${WORKSPACE}/docker-mlb-scores
 
